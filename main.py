7fccf86cbd7d025f15a133f432c0a7a08d193a93
YanKelvin <yan.kelvin@hotmail.com>
2020-10-30 22:44:08 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import sys
+import glob
+
+walk_dir = sys.argv[1]
+
+condition = "coverlet.collector"
+replace = 'Version="1.0.1"'
+value = 'Version="1.0.2"'
+
+print('walk_dir = ' + walk_dir)
+print()
+
+for filename in glob.iglob(walk_dir + '**/**', recursive=True):
+    if filename.find(".csproj") > 0 and filename[-1] == "j":
+        print(filename)
+
+        my_file = open(filename, "r")
+        lines_of_file = my_file.readlines()
+
+        for index, line in enumerate(lines_of_file):
+            if line.find(condition) > 0:
+                print(line)
+                lines_of_file[index] = line.replace(
+                    replace, value)
+
+        my_file = open(filename, "w")
+        my_file.writelines(lines_of_file)
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Procura no csproj e altera de acordo com uma condição
+
+Ex.: No código atual está buscando a linha que tiver o "coverlet.collector" e depois substitui o valor do replace pelo valor da variável value
+
+Para executar basta usar o py main.py CAMINHO_PASTA_RAIZ
